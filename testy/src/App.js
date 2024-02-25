c06f2e5980f9fc8b7cfbe52b9e8947571cde1b27
ryandw7 <134408930+ryandw7@users.noreply.github.com>
2024-02-25 20:27:33 +0000

    create react test app

---
+++ testy/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Container1 from './Containers/Container1'
+function App() {
+  return (
+    <div>
+      <Container1 />
+    </div>
+  );
+}
+
+export default App;
---
+++ testy/src/Containers/Container1.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Presentational11 from '../Presentational/Cont1/Presentational11';
+import Presentational12 from '../Presentational/Cont1/Presentational12';
+import Presentational13 from '../Presentational/Cont1/Presentational13';
+export default function Container1(){
+
+
+    return (
+        <div>
+            <Presentational11 />
+            <Presentational12 />
+            <Presentational13 />
+        </div>
+    )
+}
